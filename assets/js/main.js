77b441415e15ac17691875a3b422ce5ece8b7e99
sskkfmnn <sskkfmnn@gmail.com>
2023-02-16 14:00:34 +0100

    Lev_1_5

---
+++ assets/js/main.js
@@ -0,0 +1,19 @@
+console.log("");
+
+let x = 150;
+console.log(x);
+
+let y = 50;
+console.log(y);
+
+let z = x+y;
+console.log(z);
+
+let firstName = "John";
+console.log(firstName);
+
+let lastName = "Doe";
+console.log(lastName);
+
+let age = 35;
+console.log(age);
